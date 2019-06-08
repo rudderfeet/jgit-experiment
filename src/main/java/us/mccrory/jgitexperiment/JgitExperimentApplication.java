6420ed96e123dcf5bae7ab5b30052f6327049d4e
Scott McCrory <scott@mccrory.us>
2019-06-07 20:55:04 -0400

    Initial import

---
+++ src/main/java/us/mccrory/jgitexperiment/JgitExperimentApplication.java
@@ -0,0 +1,13 @@
+package us.mccrory.jgitexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgitExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgitExperimentApplication.class, args);
+	}
+
+}
