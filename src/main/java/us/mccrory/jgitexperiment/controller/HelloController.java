3c82227d61c81918b3a0ccae5925923c7635363e
Scott McCrory <scott@mccrory.us>
2019-06-07 22:00:10 -0400

    Basic access to GitHub works enough to read a commit log

--- src/main/java/us/mccrory/jgitexperiment/controller/HelloController.java
+++ src/main/java/us/mccrory/jgitexperiment/controller/HelloController.java
@@ -1,39 +1,82 @@
 package us.mccrory.jgitexperiment.controller;
 
 import java.io.File;
-import java.util.Iterator;
+import java.io.IOException;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() throws IllegalStateException, GitAPIException {
-    	
-    	File repoDir = new File("/Users/smccrory/Applications/jGit/jgit-experiment");    	
-    	Git git = Git.cloneRepository()
-    			  .setURI("https://github.com/rudderfeet/jgit-experiment.git")
-    			  .setDirectory(repoDir)
-    			  .call();
-    	
-    	RevWalk walk = new RevWalk(git.getRepository());
-    	
-    	StringBuffer message = new StringBuffer();
-		for (Iterator<RevCommit> revWalkIter = walk.iterator(); revWalkIter.hasNext();) {
-			RevCommit revCommit = (RevCommit) revWalkIter.next();
-			message.append(revCommit.getShortMessage());
-			message.append("<BR/>");		
+	@RequestMapping("/")
+	public String index() throws IllegalStateException, GitAPIException, RevisionSyntaxException, MissingObjectException, IncorrectObjectTypeException, AmbiguousObjectException, IOException {
+
+		File repoDir = new File("/Users/smccrory/Applications/jGit/jgit-experiment");
+		Git git = Git.cloneRepository().setURI("https://github.com/rudderfeet/jgit-experiment.git")
+				.setDirectory(repoDir).call();
+		Repository repository = git.getRepository();
+
+		StringBuffer message = new StringBuffer();
+		Iterable<RevCommit> logs = git.log().call();
+
+		int count = 0;
+		for (RevCommit rev : logs) {
+			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
+			// " + rev.getId().getName() */);
+			count++;
 		}
-		
-		walk.close();
-    	
-        return message.toString();
-    }
+		System.out.println("Had " + count + " commits overall on current branch");
+
+		logs = git.log().add(repository.resolve(git.getRepository().getFullBranch())).call();
+		count = 0;
+		for (RevCommit rev : logs) {
+			System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id: " + rev.getId().getName() */);
+			count++;
+		}
+		System.out.println("Had " + count + " commits overall on " + git.getRepository().getFullBranch());
+
+		logs = git.log().all().call();
+		count = 0;
+		for (RevCommit rev : logs) {
+			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
+			// " + rev.getId().getName() */);
+			count++;
+		}
+		System.out.println("Had " + count + " commits overall in repository");
+
+		logs = git.log()
+				// for all log.all()
+				.addPath("README.md").call();
+		count = 0;
+		for (RevCommit rev : logs) {
+			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
+			// " + rev.getId().getName() */);
+			count++;
+		}
+		System.out.println("Had " + count + " commits on README.md");
+
+		logs = git.log()
+				// for all log.all()
+				.addPath("pom.xml").call();
+		count = 0;
+		for (RevCommit rev : logs) {
+			// System.out.println("Commit: " + rev /* + ", name: " + rev.getName() + ", id:
+			// " + rev.getId().getName() */);
+			count++;
+		}
+		System.out.println("Had " + count + " commits on pom.xml");
+
+		return message.toString();
+
+	}
 
 }
